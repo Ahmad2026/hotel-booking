87aa71140118313eec9f823288ba5a79f7c96873
Ahad <shaikhahad199@gmail.com>
2020-02-13 13:36:11 +0530

    Ahad commit

---
+++ Hdesc.php
@@ -0,0 +1,57 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="Hdesc.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+
+<body>
+
+    <section>
+        <div class="container" style="background:#ff123ffe">
+            <div class="card">
+                <div class="himage">
+                    <img src="images/h1.jpg" alt="">
+                </div>
+                <div class="box" style="background:ff123ffe">
+                    <p>On entering this charming hotel in Florence, you will immediately sense its special intimate atmosphere that makes you feel like being in your own florentine home. Each detail has been passionately chosen and each room deserves a visit. Hotel Cellai style mixes valuable antiques and original artworks with an unexpected eclectic contemporary twist. The entire House recalls the ancient ‘Palazzi’ of times past where young European aristocrats lived while discovering the beauty and the artistic mysteries of Italy.</p>
+                </div>
+
+            </div>
+        </div>
+        <section>
+            <div class="container" style="background:#ff801ffd">
+                <div class="box-title text-center">
+                    <h1>Room Facility</h1>
+                </div>
+
+                <div class=" box-body" style="font-size:30px;padding:10px  ">
+                    <div class="d-flex flex-row bd-highlight mb-3">
+                        <div class="fa fa-wifi">free wifi</div>
+                        <div class="fa fa-bathtub">Bathtub</div>
+                        <i class="fas fa-parking"></i>
+                        <i class="fa fa-snowflake-o">Air conditioned</i>
+                        <i class="fa fa-taxi">Airport Transfer</i>
+                    </div>
+
+                </div>
+            </div>
+        </section>
+
+    </section>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</body>
+
+</html>
