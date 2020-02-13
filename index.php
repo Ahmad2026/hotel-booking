976f76935f1e744da965748c97469dc09f3a9ca7
Shaikh Ahmad <ahmadazmi2026@gmail.com>
2020-02-13 14:07:12 +0530

    create date picker

--- index.php
+++ index.php
@@ -1,105 +1,157 @@
+<?php
+include "config.php";
+include "session.php";
+?>
 <!DOCTYPE html>
-<html lang="en">
+<html>
 
 <head>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+  <title></title>
+  <style type="text/css">
+    .card-group {
+      margin-left: 150px;
+      margin-top: 90px;
 
-	<title></title>
 
-	<!-- Google font -->
-	<link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet">
+    }
 
-	<!-- Bootstrap -->
-	<link type="text/css" rel="stylesheet" href="css/bootstrap.min.css" />
+    .card {
+      margin-right: 50px;
+      width: 50px;
+    }
 
-	<!-- Custom stlylesheet -->
-	<link type="text/css" rel="stylesheet" href="css/style.css" />
+    .star {
+      color: yellow;
+    }
 
-	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-	<!--[if lt IE 9]>
-		  <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-		  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-		<![endif]-->
+    .navbar {
+      position: fixed;
+      top: 0;
+      width: 100%;
+      z-index: 1
+    }
+
+    body {
+      height: 1200px;
+    }
+
+    .price {
+      font-size: 14px;
+    }
+  </style>
 
 </head>
 
 <body>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-primary" style="background-color: #e3f2fd;">
+    <a class="navbar-brand" href="index.php">TAAAS</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
 
-	<div class="container">
-		<div class="row">
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+          <a class="nav-link" href="index.php">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="package.php">Packages</a>
+        </li>
+    </div>
+    <form class="form-inline my-5 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btnutline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+    <a href="profile.php"><img src="img/user.png" alt="user" width='30' height='30' style='margin-left: 10px;'></a>
+    </div>
+  </nav>
 
-		</div>
-		<div class="col-md-6 col-md-push-3">
-			<div class="booking-form ">
-				<form>
-					<div class="form-group">
-						<span class="form-label">Your Destination</span>
-						<input class="form-control" type="text" placeholder="Enter a destination or hotel name">
-					</div>
-					<div class="row">
-						<div class="col-sm-6">
-							<div class="form-group">
-								<span class="form-label">Check In</span>
-								<input type="date" min="<?php echo date("Y-m-d", strtotime("now")) ?>" value="<?php echo date("Y-m-d", strtotime("now")) ?>" class="form-control" required>
-								<!-- <input type="date" id="start" name="trip-start" value="2020-02-13" min="2020-03-13"> -->
-							</div>
-						</div>
-						<div class="col-sm-6">
-							<div class="form-group">
-								<span class="form-label">Check out</span>
-								<input class="form-control" type="date" min="<?php echo date("Y-m-d", strtotime(" +1 day")); ?>" value="<?php echo date("Y-m-d", strtotime(" +1 day")); ?>" required>
-							</div>
-						</div>
-					</div>
-					<div class="row">
-						<div class="col-sm-4">
-							<div class="form-group">
-								<span class="form-label">Rooms</span>
-								<select class="form-control">
-									<option>1</option>
-									<option>2</option>
-									<option>3</option>
-								</select>
-								<span class="select-arrow"></span>
-							</div>
-						</div>
-						<div class="col-sm-4">
-							<div class="form-group">
-								<span class="form-label">Adults</span>
-								<select class="form-control">
-									<option>1</option>
-									<option>2</option>
-									<option>3</option>
-								</select>
-								<span class="select-arrow"></span>
-							</div>
-						</div>
-						<div class="col-sm-4">
-							<div class="form-group">
-								<span class="form-label">Children</span>
-								<select class="form-control">
-									<option>0</option>
-									<option>1</option>
-									<option>2</option>
-								</select>
-								<span class="select-arrow"></span>
-							</div>
-						</div>
-					</div>
-					<div class="form-btn">
-						<button class="submit-btn">Check availability</button>
-					</div>
-				</form>
-			</div>
-		</div>
-	</div>
-	</div>
-	</div>
-	</div>
+
+  <div class="container-fluid">
+
+    <div class="card-group">
+      <?php
+      $sql = "SELECT * FROM hotel_details";
+      $result = mysqli_query($conn, $sql);
+
+      if (mysqli_num_rows($result) > 0) {
+        while ($row = mysqli_fetch_assoc($result)) {
+          $inc = 4;
+          $inc = ($inc == 4)  ? 1 : $inc + 1;
+          if ($inc == 4) echo "<div class='row'>";
+      ?>
+          <div class="col-xs-6 col-sm-6 col-lg-3">
+
+            <div class="card" style="width:100%;margin-top:20px;box-shadow:3px 3px 8px 1px #00000020 ">
+              <img class="card-img-top" src="./admin/upload/<?php echo $row['hotel_image']; ?>" height="200px">
+              <div class="card-body">
+                <h5 class="card-title" style="text-overflow: ellipsis;white-space: nowrap;overflow:hidden;"><?php echo $row['hotel_name'] ?></h5>
+                <p class="card-text" style="text-overflow: ellipsis;white-space: nowrap;overflow:hidden;"> <?php echo $row['hotel_description'] ?> </p>
+                <h5 class="price">
+                  <div class="font-weight-bold d-inline">Price</div> <?php echo $row['price'] ?>
+                </h5>
+                <h5 class="price">
+                  <div class="font-weight-bold d-inline">Location</div> <?php echo $row['hotel_location'] ?>
+                </h5>
+                <?php
+                if ($row['rating'] == 1) {
+                  for ($i = 0; $i < $row['rating']; $i++) {
+                  }
+                } else if ($row['rating'] == 2) {
+                  for ($i = 0; $i < $row['rating']; $i++) {
+                    echo "<i class='fa fa-star star'></i>";
+                  }
+                } else if ($row['rating'] == 3) {
+                  for ($i = 0; $i < $row['rating']; $i++) {
+                    echo "<i class='fa fa-star star'></i>";
+                  }
+                } else if ($row['rating'] == 4) {
+                  for ($i = 0; $i < $row['rating']; $i++) {
+                    echo "<i class='fa fa-star star'></i>";
+                  }
+                } else if ($row['rating'] == 5) {
+                  for ($i = 0; $i < $row['rating']; $i++) {
+                    echo "<i class='fa fa-star star'></i>";
+                  }
+                }
+                ?>
+                <br>
+                <a href="date_picker.php?id=<?php echo $row['hotel_id'] ?>" class="btn btn-primary">Book Now</a>
+              </div>
+            </div>
+
+          </div>
+      <?php
+
+          if ($inc == 4) echo "</div>";
+        }
+        if ($inc == 4) echo "<div class='col-sm-3><div class='col-sm-3></div>";
+      }
+
+      ?>
+    </div>
+
+
+    <nav aria-label="Page navigation example">
+      <ul class="pagination justify-content-center mt-5">
+        <li class="page-item disabled">
+          <a class="page-link" href="#" tabindex="-1" aria-disabled="true">Previous</a>
+        </li>
+        <li class="page-item"><a class="page-link" href="http://localhost/hotel.php?#">1</a></li>
+        <li class="page-item"><a class="page-link" href="http://localhost/hotel1.php?#">2</a></li>
+        <li class="page-item"><a class="page-link" href="#">3</a></li>
+        <li class="page-item">
+          <a class="page-link" href="http://localhost/hotel1.php?#">Next</a>
+        </li>
+      </ul>
+    </nav>
+  </div>
+
+  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 </body>
 
 </html>
